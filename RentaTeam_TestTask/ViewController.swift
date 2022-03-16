5a45a9e794005d329c1a3278357b17a62cad26f4
Alexander Fomin <inzhener@outlook.com>
2022-03-16 18:07:59 +0300

    Initial Commit

---
+++ RentaTeam_TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RentaTeam_TestTask
+//
+//  Created by Александр Фомин on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
